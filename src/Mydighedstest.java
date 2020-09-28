bec166fcd3ca428f4d45d52a6f6fd14f3ae3e9e0
Your Name <you@example.com>
2020-09-28 14:42:44 +0200

    Konflikt

---
+++ src/Mydighedstest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Mydighedstest {
+    public static void main (String[]args){
+        System.out.println("Er du myndig? Indtast din alder:");
+
+        int alder;
+        Scanner input =new Scanner(System.in);
+        alder = input.nextInt();
+
+        if (alder >= 18){
+            System.out.println("Du er myndig");
+        }
+
+        else {
+            System.out.println("Du er ikke myndig");
+        }
+
+    }
+}
